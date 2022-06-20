c0689fe70c2a8bfcd01c68b36aa51fcff40682f4
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-06-20 14:27:59 -0500

    Fixing providers page issues

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from "react";
-import { useSearchParams, useLocation } from "react-router-dom";
-import { searchShows } from "../services/tmdb-api";
+import { useSearchParams } from "react-router-dom";
 import TitleList from "./TitleList";
 
-const Pagination = ({ currentPageNumber, pageLimit, initialResults, watchList, toggle }) => {
+const Pagination = ({ currentPageNumber, pageLimit, searchResults, watchList, toggle }) => {
   const [currentPage, setCurrentPage] = useState(currentPageNumber);
-  const [searchResults, setSearchResults] = useState(initialResults);
   const [params, setParams] = useSearchParams();
-  const location = useLocation();
+  const query = params.get("query");
 
   const nextPage = () => {
     setCurrentPage((prevState) => prevState + 1);
@@ -17,54 +15,25 @@ const Pagination = ({ currentPageNumber, pageLimit, initialResults, watchList, t
     setCurrentPage((prevState) => prevState - 1);
   };
 
-  const changePage = (e) => {
-    const pageNumber = e.target.textContent;
-    setCurrentPage(pageNumber);
-  };
-
-  const getPaginationGroup = () => {
-    let start = Math.floor((currentPage - 1) / 5) * 5;
-    return new Array(5).fill().map((_, index) => start + index + 1);
-  }
-
   useEffect(() => {
     params.set("page", currentPage);
     setParams(params);
   }, [currentPage]);
 
-  useEffect(() => {
-    console.log("this should work");
-    const query = params.get("query");
-    const page = params.get("page");
-    if (query !== "") {
-      if (!(page > pageLimit) && !(page < 1)) {
-        searchShows(query, page).then(response => setSearchResults(response.results));
-      }
-    }
-  }, [location]);
-
   return (
     <>
-    <TitleList name="Results" titles={searchResults} watchList={watchList} toggle={toggle}/>
-    {currentPage === pageLimit && 
-      <h1>End of Results</h1>
-    }
-    <div className="pagination">
-      {currentPage > 1 && 
-        <button onClick={previousPage} className="previous">Prev</button>
+      <TitleList name={`Results for query "${query}"`} titles={searchResults} watchList={watchList} toggle={toggle}/>
+      {(currentPage >= pageLimit || !pageLimit) &&
+        <h2 className="end">End of Results</h2>
       }
-      {/* {getPaginationGroup().map((item, index) => {
-        return <button
-          key={index}
-          onClick={(event) => changePage(event)}
-        >
-          {item}
-        </button>
-      })} */}
-      {currentPage < pageLimit &&
-        <button onClick={nextPage} className="next">Next</button>
-      }
-    </div>
+      <div className="pagination">
+        {(currentPage > 1 && currentPage <= pageLimit) &&
+          <button onClick={previousPage} className="previous">Prev</button>
+        }
+        {currentPage < pageLimit &&
+          <button onClick={nextPage} className="next">Next</button>
+        }
+      </div>
     </>
   );
 };
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { searchShows } from '../services/tmdb-api';
-import TitleList from '../components/TitleList';
 import Pagination from "../components/Pagination";
 
 const SearchPage = ({ watchList, toggle }) => {
@@ -12,12 +11,12 @@ const SearchPage = ({ watchList, toggle }) => {
   const page = params.get('page');
 
   useEffect(() => {
-    if (query) {
+    if (query !== "") {
       searchShows(query, page).then(response => {
         setData(response);
       });
     }
-  }, [query]);
+  }, [location]);
 
   return (
     <>
@@ -25,7 +24,7 @@ const SearchPage = ({ watchList, toggle }) => {
         <Pagination
           currentPageNumber={data.page}
           pageLimit={data.total_pages}
-          initialResults={data.results}
+          searchResults={data.results}
           watchList={watchList}
           toggle={toggle}
         />
